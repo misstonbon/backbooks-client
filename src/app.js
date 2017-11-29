a57a90a364c68a2382376ffa4f9e45a329172e2c
Tanja Stroble <plusdeltah@gmail.com>
2017-11-29 09:41:38 -0800

    renders and saves book list

--- src/app.js
+++ src/app.js
@@ -1,129 +1,101 @@
-
+// app.js
 import $ from 'jquery';
 import _ from 'underscore';
+
 import 'foundation-sites/dist/css/foundation.css';
 import './style.css';
+
 import Book from './models/book';
-import Cat from './models/cat';
 import BookList from './collections/book_list';
-import CatList from './collections/cat_list';
-
-const codingInterview = new Book({
-  title: "Cracking the Coding Interview",
-  author: "Gale",
-  publication_year: 2011,
-});
-
-const jeeves = new Cat({
-  name: "Jeeves",
-  human: "Shaunna",
-  species: "Just kidding, I'm a dog.",
-});
 
 
-const rawBookData = [
-  {
-    title: 'Practical Object-Oriented Design in Ruby',
-    author: 'Sandy Metz',
-    publication_year: 2012
-  }, {
-    title: 'Parable of the Sower',
-    author: 'Octavia Butler',
-    publication_year: 1993
-  }, {
-    title: 'A Wizard of Earthsea',
-    author: 'Ursula K. Le Guin',
-    publication_year: 1969
-  }
-];
-
-const bookList = new BookList(rawBookData);
-bookList.add(codingInterview);
-
-bookList.add({
-  title: "Gatsby",
-  author: "Fitzgerald",
-  publication_year: 1922,
-});
-
-bookList.forEach((book) => {
-  console.log(`${book.get('title')} by ${book.get('author')}`);
-});
-
-const newBooks = bookList.filter((book) => book.get('publication_year') > 2000);
-
-newBooks.forEach((book) => {
-  console.log(`AAAAAAAAAAA ${book.get('title')} is a newer book !!`)
-});
-
-const shaunnasCat = new Cat({
-  name: "Jeeves",
-  human: "Shaunna",
-  species: "Just kidding, I'm a dog.",
-});
-const rawCatData = [
-  {
-  name: "Some cat",
-  human: "Chris",
-},
-  {
-  name: "Jerry",
-  human: "Tanja",
-}
-]
-
-const catList = new CatList(rawCatData);
-catList.add(jeeves);
-
-catList.add({
-  name: "Riley",
-  human: "Dee",
-});
-
-catList.forEach((cat) => {
-  console.log(` CAAAAAAAAAAAATS ${cat.get('name')}`);
-});
-
-console.log(catList);
-console.log(catList.at(1).get('name'));
-
-console.log(bookList);
-console.log(bookList.at(2).get('title'));
-
-// codingInterview.attributes.title = "Cracking the Whiteboarding Interview";
-// never change attrs directly
-// bypasses validations
-// codingInterview.attributes to read attrs
-// get and set in backbone
-console.log(`Title is ${codingInterview.get('title')}`);
-
-codingInterview.set('title','The lord of the flies');
-//this will change the title and alert any listeners of the change
-
-
-// Starts undefined - we'll set this in $(document).ready
-// once we know the template is available
+const bookList = new BookList();
 let bookTemplate;
 
 const render = function render(bookList) {
 
-  //get alement to append to
+  // Get the element to append to
   const $bookList = $('#book-list');
   $bookList.empty();
-  bookTemplate = _.template($('#book-template').html());
-  //build forEach loop;
+
   bookList.forEach((book) => {
     $bookList.append(bookTemplate(book.attributes));
-    // $bookList.append(bookTemplate(book.toJSON);
-    //can also do it this way ;
   });
+};
+
+// Listening for a Backbone Event
+
+// 1.  Create an Event Listener
+// const bogusListener = function bogusListener(event)  {
+//   console.log('Event Occured!');
+// };
+
+// 2.  Register the Event Handler with the Component
+// bookList.on('bogus', bogusListener);
+
+// 3.  Trigger the event
+// bookList.trigger('bogus', 'Argument!');
+
+const fields = ['title', 'author', 'publication_year'];
+
+const events = {
+  addBook(event) {
+    event.preventDefault();
+    const bookData = {};
+    fields.forEach( (field) => {
+      bookData[field] = $(`input[name=${field}]`).val();
+    });
+    const book = new Book(bookData);
+    bookList.add(book);
+    book.save({}, {
+      success: events.successfullSave,
+      error: events.failedSave,
+    });
+  },
+  sortBooks(event) {
+    $('.current-sort-field').removeClass('current-sort-field');
+    $(this).addClass('current-sort-field');
+
+    // Get the class list of the selected element
+    const classes = $(this).attr('class').split(/\s+/);
+
+    classes.forEach((className) => {
+      if (fields.includes(className)) {
+        if (className === bookList.comparator) {
+          bookList.models.reverse();
+          bookList.trigger('sort', bookList);
+        }
+        else {
+          bookList.comparator = className;
+          bookList.sort();
+        }
+      }
+    });
+  },
+  successfullSave(book, response) {
+    $('#status-messages ul').empty();
+    $('#status-messages ul').append(`<li>${book.get('title')} added!</li>`);
+    $('#status-messages').show();
+  },
+  failedSave(book, response) {
+    $('#status-messages ul').empty();
+    for(let key in response.responseJSON.errors) {
+      response.responseJSON.errors[key].forEach((error) => {
+        $('#status-messages ul').append(`<li>${key}:  ${error}</li>`);
+      })
+    }
+    $('#status-messages').show();
+    book.destroy();
+  },
 };
 
 
 $(document).ready(() => {
   bookTemplate = _.template($('#book-template').html());
+  $('#add-book-form').submit(events.addBook);
+  $('.sort').click(events.sortBooks);
+  bookList.on('update', render, bookList);
+  bookList.on('sort', render, bookList);
 
-  $('#bookList').append(bookTemplate(codingInterview.attributes));
-  $('#bookList').append(bookTemplate(codingInterview.toJSON()));
-  render (bookList);
+  bookList.fetch();
 });
--- src/collections/book_list.js
+++ src/collections/book_list.js
@@ -2,7 +2,18 @@ import Backbone from 'backbone';
 import Book from '../models/book';
 
 const BookList = Backbone.Collection.extend({
-model: Book
+  model: Book,
+  url: 'http://localhost:3000/books',
+
+  // we need to override parse b/c our API returns
+  // data in a weird format
+  parse: function(response) {
+    return response["books"];
+  },
+
+
+
+  comparator: 'title',
 });
 
 export default BookList;
